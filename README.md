68367816620c7098bb727815c4dc5b68ca22f732
akihito <takeda.akihito@gmail.com>
2014-04-30 17:56:30 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Web::API::Mock - It's new $module
+
+# SYNOPSIS
+
+    use Web::API::Mock;
+
+# DESCRIPTION
+
+Web::API::Mock is ...
+
+# LICENSE
+
+Copyright (C) akihito.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+akihito <takeda.akihito@gmail.com>
---
+++ lib/Web/API/Mock.pm
@@ -0,0 +1,39 @@
+package Web::API::Mock;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Web::API::Mock - It's new $module
+
+=head1 SYNOPSIS
+
+    use Web::API::Mock;
+
+=head1 DESCRIPTION
+
+Web::API::Mock is ...
+
+=head1 LICENSE
+
+Copyright (C) akihito.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+akihito E<lt>takeda.akihito@gmail.comE<gt>
+
+=cut
+
